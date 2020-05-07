d81fc519d2108b310dc501b079d7362fe2d88c49
frangusdj <frangusdj@hotmail.com>
2020-05-06 19:48:45 -0500

    Solo esta declarado en BNF

---
+++ src/tdaconjunto/TDAConjunto.java
@@ -0,0 +1,34 @@
+package tdaconjunto;
+/**************************************************
+ * Elaborar un BNF con conjunto y sus respectivos *
+ * comportamientos. Luego plasmarlo en la progra- *
+ * mación utilizando un lenguaje cualquiera       *
+ * Fecha: 06/05/2020                              *
+ * @author Franklin G. Mendoza                    *
+ **************************************************/
+
+/***************************************************************
+ *BNF Conjunto                                                 *
+ *<conjunto>:=<digito> | {<digito>} | <caracter> | {<caracter>}*
+ *<digito>:= 0 | 1 | 2 | 3 |4 | 5 | 6 | 7 | 8 | 9              *
+ *<caracter>:= 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'|'I'|'J'|'K'|'L' *
+ *            |'M'|'N'|'O'|'P'|'Q'|'R'|'S'|'T'|'U'|'V'|'W'|'X' *
+ *            |'Y'|'Z'|'a'|'b'|'c'|'d'|'e'|'f'|'g'|'h'|'i'|'j' *
+ *            |'k'|'l'|'m'|'n'|'o'|'p'|'q'|'r'|'s'|'t'|'u'|'v' *
+ *            |'w'|'x'|'y'|'z'                                 *
+ * <fin_caracter>:='\0'                                        *
+ * Comportamientos                                             *
+ * 1.- Union                                                   *
+ * 2.- Intersección                                            *
+ * 3.- Diferencia                                              *
+ ***************************************************************/
+public class TDAConjunto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
